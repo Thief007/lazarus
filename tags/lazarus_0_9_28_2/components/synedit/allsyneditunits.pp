75967e8b2cd2016eb178d50c2caa6ca1e6298b9a
vincents <vincents@4005530d-fff6-0310-9dd1-cebe43e6787f>
2009-10-24 18:56:47 +0000

    git-svn-id: http://svn.freepascal.org/svn/lazarus@22277 4005530d-fff6-0310-9dd1-cebe43e6787f

---
+++ tags/lazarus_0_9_28_2/components/synedit/allsyneditunits.pp
@@ -0,0 +1,48 @@
+{  $Id$  }
+{
+ /***************************************************************************
+                               allsyneditunits.pp
+
+                      dummy unit to compile all units 
+
+ /***************************************************************************
+}
+unit AllSynEditUnits;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  SynTextDrawer, SynEditKeyCmds, SynEditTypes, SynEditStrConst,
+  SynEditSearch, SynEditMiscProcs, SynEditmiscClasses, SynEditTextbuffer,
+  SynEdit, SynEditHighlighter, SynCompletion, SynEditAutoComplete, 
+  SynEditLazDsgn, SynRegExpr, SynEditRegexSearch, SynEditExport, 
+  SynExportHTML, SynMemo, SynMacroRecorder, SynEditPlugins,
+  SynPluginSyncronizedEditBase, SynPluginTemplateEdit, SynPluginSyncroEdit,
+  SynHighlighterAny,
+  SynhighlighterCPP, 
+  SynHighlighterCss, 
+  SynHighlighterHashEntries, 
+  SynhighlighterHTML, 
+  SynHighlighterJava,
+  SynHighlighterJScript,
+  SynHighlighterLFM, 
+  SynHighlighterMulti,
+  SynHighlighterPas,
+  SynHighlighterPerl, 
+  SynHighlighterPHP,
+  SynHighlighterPosition, 
+  SynHighlighterPython, 
+  SynHighlighterSQL,
+  SynHighlighterTeX, 
+  SynHighlighterUNIXShellScript, 
+  SynHighlighterVB, 
+  SynHighlighterXML,
+  SynGutter, SynGutterChanges, SynGutterCodeFolding, SynGutterLineNumber, SynGutterMarks,
+  SynPropertyEditObjectList, SynDesignStringConstants;
+
+implementation
+
+end.
+
